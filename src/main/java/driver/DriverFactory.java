08bb9fe881b6f5b5dd127fb58765ccc2569bbbcd
Varalakshmi0904 <varalakshmi0904@GMAIL.COM>
2025-11-20 21:12:10 +0530

    added config file

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -1,26 +1,30 @@
 package driver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
 	private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
-	
+
 	public static WebDriver getDriver() {
-		if(webDriver.get()==null) {
+		if (webDriver.get() == null) {
 			webDriver.set(createDriver());
 		}
 		return webDriver.get();
 	}
-	
 
 	private static WebDriver createDriver() {
 		WebDriver driver = null;
 
-		String browserType = "chrome";
-		switch (browserType) {
+		switch (getBrowserType()) {
 		case "chrome": {
 			ChromeOptions chromeoptions = new ChromeOptions();
 			chromeoptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
@@ -28,15 +32,31 @@ public class DriverFactory {
 			break;
 		}
 		case "firefox": {
-			ChromeOptions chromeoptions = new ChromeOptions();
-			chromeoptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-			driver = new ChromeDriver(chromeoptions);
+			FirefoxOptions firefoxoptions = new FirefoxOptions();
+			firefoxoptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+			driver = new FirefoxDriver(firefoxoptions);
 			break;
 		}
 		}
 		driver.manage().window().maximize();
 		return driver;
 	}
+
+	public static String getBrowserType() {
+		String browserType = null;
+		try {
+
+			Properties properties = new Properties();
+			FileInputStream fis = new FileInputStream(
+					System.getProperty("user.dir") + "/src/main/java/properties/config.properties");
+			properties.load(fis);
+			browserType = properties.getProperty("browser").toLowerCase().trim();
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+		return browserType;
+	}
+
 	public static void cleanupDriver() {
 		webDriver.get().quit();
 		webDriver.remove();
