b2dbcae043a1a42514e3e1679224f652ad007053
Varalakshmi0904 <varalakshmi0904@GMAIL.COM>
2025-11-20 22:29:05 +0530

    edited runner

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -1,20 +1,26 @@
 package runners;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features", 
-               //tags = "@Graph and @ArrayList",
-				tags = "@smoke_Ll",
-				glue= {"stepDefinitions","base"}, 
-				monochrome=true, dryRun=false,
-			//plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+@CucumberOptions(features = "src/test/resources/features",
+		// tags = "@Graph and @ArrayList",
+		tags = "@smoke_Ll", glue = { "stepDefinitions", "base" }, monochrome = true, dryRun = false,
+		// plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+
+	//	plugin= {"pretty", "html:target/cucmber", "json:target/cucumber.json"})
+
+		plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "html:target/ranani.html","json:target/cucumber.json" })
+
+public class MainRunner extends AbstractTestNGCucumberTests {
+
 	
-
-			//	plugin= {"pretty", "html:target/cucmber", "json:target/cucumber.json"})
-
-				plugin= {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","html:target/ranani.html", "json:target/cucumber.json"})
-                
-public class MainRunner extends AbstractTestNGCucumberTests{
+	  @Override
+	  
+	  @DataProvider(parallel=true) public Object[][] scenarios(){ return
+	  super.scenarios(); }
+	 
 
 }
