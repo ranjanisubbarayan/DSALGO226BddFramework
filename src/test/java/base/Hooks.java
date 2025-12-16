189ac3c0e05b747feea046bab6086944078e85c0
Ranjanisubbarayan <ranjanibala81@gmail.com>
2025-12-16 15:01:22 -0500

    webdriver init

--- src/test/java/base/Hooks.java
+++ src/test/java/base/Hooks.java
@@ -1,40 +1,53 @@
 package base;
 
-
-import com.aventstack.extentreports.ExtentTest;
-
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
+import static driver.DriverFactory.getDriver;
+import static driver.DriverFactory.cleanupDriver;
+
 import utilities.Report;
-
-
-
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.ExtentTest;
 
 public class Hooks {
 
-
     public static ExtentTest test;
- 
 
-    @Before    
+    @Before
     public void setUp(Scenario scenario) {
 
-            Report.getInstance().createTest(scenario.getName());
+        getDriver();
+
+       
+        test = Report.getInstance().createTest(scenario.getName());
     }
 
-
-    
     @After
-    public static void tearDown() {
-    	
-        Report.getInstance().flush();  
+    public void tearDown(Scenario scenario) {
+       
+        if (scenario.isFailed()) {
+            try {
+             
+                byte[] screenshot = ((org.openqa.selenium.TakesScreenshot) getDriver())
+                        .getScreenshotAs(org.openqa.selenium.OutputType.BYTES);
+
+              
+                test.fail("Scenario Failed",
+                        MediaEntityBuilder.createScreenCaptureFromBase64String(
+                                java.util.Base64.getEncoder().encodeToString(screenshot)
+                        ).build());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+      
+        cleanupDriver();
+
+      
+        Report.getInstance().flush();
     }
-
-
-
-
 }
-
